f11ffdd0820775f23709a061cd3351d4bab3aff6
umeshmehta6551 <148803637+umeshmehta6551@users.noreply.github.com>
2024-07-30 12:44:10 +0530

    Add files via upload
    this is My Day 2 on Python Data Science

---
+++ day2.py
@@ -0,0 +1,32 @@
+# #this is my second day 
+# var =10
+# print(" the  value of  var is =", var)
+# print(type(var))
+
+# A=10
+# B=8.6
+# C=1.4
+# print(A+B+C)
+# st ="MY name is Umesh Mehta"
+# print(st)
+# stt='Umesh'
+# print(stt[3])
+# print(stt[1:4])
+# for get sub group of a group 
+"""  thi is used for multiline comments """
+
+# st="upflairs" 
+# # print(st[::3])
+# print(st[::-1])
+
+st=" MY Name Is Umesh Mehta  from Kota "
+# print(st[12:18])
+
+# print(len(st))
+print (st.count("M"))
+
+
+
+
+
+
