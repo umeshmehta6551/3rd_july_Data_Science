c58b7c4482c520536d2a8f44f98b0d6d20dd76a3
umeshmehta6551 <148803637+umeshmehta6551@users.noreply.github.com>
2024-07-31 10:47:02 +0530

    Add files via upload
    Day 3rd of  Data Science With ML & Ai

---
+++ day3.py
@@ -0,0 +1,98 @@
+###___________BOOLEAN<<<&<<<<LIST_____________###
+
+
+# var1=True
+# var2=False
+# print(var1)
+# print(var2)
+# print( type(var1),(var2))
+
+"""
+Array is a collection of similar data type and we 
+can store multiple values is this .
+"""
+
+# static =>>>  fixed size we cant change it 
+# Dynamic =>>  it is not fixed the storage is 
+#assigened during exicution 
+
+"""
+1 static 
+2 Dynamic 
+3 refrential
+"""
+# # list 
+# ls = [10,20,30,40.5,"umesh",True]
+# print(ls)
+# print(type(ls))
+
+# ls=[10,24,30,40,50,60]
+# print(ls[3])
+# print(ls[-1])
+
+
+# for find air from ls 
+
+# ls=[5,6,7,2,"upflairs"]
+# var=ls[4]
+# print(var[4:7])
+
+# mutable = changeble
+# imutable = unchangeble
+
+# student =["umesh","Nitesh","vaibhav","yajat"]
+# # print(student)
+# # student.append("Shubham")# append you for add items in the list 
+# # student[0]="Umesh_Mehta"
+# # student.pop() # for pop 
+# # student.insert(1," shubh")
+# # student.remove("vaibhav")
+# del student[2]
+# print(student)
+
+ls1=['a','b','c','d','e']
+ls2=[10,20,50,30,40,75,45]
+
+# ls1.reverse()
+# print(ls1)
+# ls2.sort()
+# print(ls2)
+
+# print(min(ls2))
+# print(max(ls2))
+# print(sum(ls2))
+
+#>>>>>>>>>>>> tuple  >>>>>>>>>>>>
+# it is immutable and we can't change it 
+
+# tpl=(10,30.5,40,"umesh")
+# print(tpl)
+# print(type(tpl))
+
+#>>>>>>>>>> sets >>>>>>>>>>>
+# st ={"umesh","umesh",20,20,20,12,24,23,}
+# st.add(500)
+# st.remove(20)
+# st.discard(5361) # it doest not get error and it remove the item 
+# print(st)
+# print(type(st))
+
+# st1={10,20,30,40,23,10,13,10}
+# st2={ 10,20,46,64,47,80}
+# st1.update(st2)
+# print(st1.intersection(st2))
+
+# dictionary <<<<<<<
+
+# marks ={'umesh':45,'yajat':20,'vaibhav':10,}
+# print(marks)
+# print(type(marks))
+
+
+
+
+
+
+
+ 
+
